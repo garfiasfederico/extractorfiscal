1d57febc555b62b92863d1a2a12c4fe32b67c4c2
Federico Sanchez <garfias.federico@gmail.com>
2025-06-10 23:32:23 -0600

    Adecuaciones

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def read_root():
 
 
 @app.get("/extract/{rfc}/{req}/{anio_inicio}/{anio_final}")
-def get_results(rfc: str, req: str, anio_inicio: int = None, anio_final: int = None):
-    resultado = declaraciones.getdeclaraanuales(rfc,anio_inicio,anio_final)
+def get_results(rfc: str, req: str, anio_inicio: int = None, anio_fin: int = None):
+    resultado = declaraciones.getdeclaraanuales(rfc,anio_inicio,anio_fin)
     return {
             "rfc": rfc, 
             "req": req, 
             "anio_inicio":anio_inicio,
-            "anio_final":anio_final,
+            "anio_final":anio_fin,
             "result":resultado["result"],
             "message":resultado["message"],
             }
