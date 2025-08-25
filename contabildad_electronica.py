fe23daf75ab7aa5d9a40e370ff5f3a17dc34e156
Federico Sanchez <garfias.federico@gmail.com>
2025-08-25 14:17:30 -0600

    Adecuaciones

--- contabildad_electronica.py
+++ contabildad_electronica.py
@@ -107,7 +107,13 @@ def getcontabilidadelectronica(rfc_c:str,inicial:int,final:int):
             print("Procedemos a realizar la consulta de los acuses correspondientes ")            
             driver.get('https://wwwmat.sat.gob.mx/operacion/16203/consulta-tus-acuses-generados-en-la-aplicacion-contabilidad-electronica') 
             time.sleep(15)
-            iframe = driver.find_element(By.ID, "iframetoload")    
+            #iframe = driver.find_element(By.ID, "iframetoload")  
+
+            iframe = WebDriverWait(driver, 15)\
+                            .until(EC.element_to_be_clickable((By.ID,
+                                                            'iframetoload')))
+
+
             WebDriverWait(driver, 30).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "iframetoload")))           
             #driver.switch_to.frame(iframe)
             
